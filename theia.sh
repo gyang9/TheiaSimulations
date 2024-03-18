04d5c2f498677f7acaa194b00967dbdbd6eb122a
Guang Yang <yangguang0229@hotmail.com>
2024-03-18 17:00:29 -0400

    Delete theia.sh

--- theia.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export THEIADATA=/exp/dune/app/users/gyang/TheiaSimulations
-export PATH=/exp/dune/app/users/gyang/TheiaSimulations/install/bin:$PATH
-export LD_LIBRARY_PATH=/exp/dune/app/users/gyang/TheiaSimulations/install/lib:$LD_LIBRARY_PATH
