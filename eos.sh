1b04dd91cd369a721226fe7a20eac6eee1526aeb
Guang Yang <yangguang0229@hotmail.com>
2024-03-18 17:00:21 -0400

    Delete eos.sh

--- eos.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export THEIADATA=/exp/dune/app/users/gyang/TheiaSimulations
-export PATH=/exp/dune/app/users/gyang/TheiaSimulations/build/bin:$PATH
-export LD_LIBRARY_PATH=/exp/dune/app/users/gyang/TheiaSimulations/build/lib:$LD_LIBRARY_PATH
